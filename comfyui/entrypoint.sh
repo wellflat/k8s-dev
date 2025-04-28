881d44f9440df4730ad5cf7193e09d4caffec7ce
wellflat <wellflat@gmail.com>
2025-04-28 13:19:11 +0900

    modified comfyui container

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 COMFYUI_PORT=8188
-python3 ComfyUI/main.py --listen 0.0.0.0 --port $COMFYUI_PORT
+LOWVRAM=1
+if [ "$LOWVRAM" -eq 1 ]; then
+    python3 ComfyUI/main.py --listen 0.0.0.0 --port $COMFYUI_PORT --lowvram
+else
+    python3 ComfyUI/main.py --listen 0.0.0.0 --port $COMFYUI_PORT
+fi
