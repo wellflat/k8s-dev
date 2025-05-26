67173a2ab19a109f69fc53c6d850b854814b29e1
wellflat <wellflat@gmail.com>
2025-05-26 17:37:30 +0900

    modified entrypoint.sh

--- comfyui/entrypoint.sh
+++ comfyui/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "${LOWVRAM}" -eq 1 ]; then
-    python3 ComfyUI/main.py --listen 0.0.0.0 --port ${COMFYUI_PORT} --lowvram
+    python3 ComfyUI/main.py --listen 0.0.0.0 --port ${COMFYUI_PORT} --novram
+elif [ "${CPU}" -eq 1 ]; then
+    python3 ComfyUI/main.py --listen 0.0.0.0 --port ${COMFYUI_PORT} --cpu
 else
     python3 ComfyUI/main.py --listen 0.0.0.0 --port ${COMFYUI_PORT}
 fi
