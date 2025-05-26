0a1ef609dad1942ddf40bbb8de1719de475e2c8c
wellflat <wellflat@gmail.com>
2025-05-26 16:27:46 +0900

    modified request.sh

--- request.sh
+++ request.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# service経由
 curl $(minikube service hello-service --url)
+
+# ingress経由
+curl http://$(minikube ip) -H 'Host: mini.trigkey.local'
+
 
 portforward() {
     kubectl port-forward --address 0.0.0.0 service/hello-service 8888:80
