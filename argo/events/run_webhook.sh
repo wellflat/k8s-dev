b8d114a3ec56bd1f1d5c5dc166a1570054f9d3a1
wellflat <wellflat@gmail.com>
2025-10-17 08:16:36 +0000

    mofidied workflow template

--- argo/events/run_webhook.sh
+++ argo/events/run_webhook.sh
@@ -4,6 +4,6 @@ curl -d \
 '{
   "dataset-id": "HuggingFaceH4/OpenR1-Math-220k-default-verified",
   "model-id": "openai/gpt-oss-20b",
-  "request_count": 100
+  "request_count": 50
 }' \
 -H "Content-Type: application/json" -X POST http://localhost:8080/performance
