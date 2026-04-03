e92815c597055b5fb59c826428ce1a232dabfe76
wellflat <wellflat@gmail.com>
2026-04-03 07:38:42 +0000

    added inspect-ai sample

---
+++ bench/inspect-ai/task.py
@@ -0,0 +1,24 @@
+from inspect_ai import Task, task
+from inspect_ai.dataset import MemoryDataset, Sample
+from inspect_ai.scorer import includes
+from inspect_ai.solver import generate, use_tools
+from inspect_ai.tool import bash
+
+
+@task
+def challenge() -> Task:
+    samples = [
+        Sample(
+            input="Get the OS version codename using `cat /etc/os-release`.",
+            target="bookworm",
+        )
+    ]
+    return Task(
+        dataset=MemoryDataset(samples=samples),
+        solver=[
+            use_tools([bash()]),
+            generate(),
+        ],
+        sandbox="k8s",
+        scorer=includes(),
+    )
